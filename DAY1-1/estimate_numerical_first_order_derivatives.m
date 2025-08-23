f6ce7ab5c373a67ed1bf139848251951454be62c
Kumaresh <51103814+kummi0402@users.noreply.github.com>
2025-08-23 17:32:10 +0900

    Update estimate_numerical_first_order_derivatives.m

--- DAY1-1/estimate_numerical_first_order_derivatives.m
+++ DAY1-1/estimate_numerical_first_order_derivatives.m
@@ -13,33 +13,38 @@ figure(1);
 plot(x, y, '-ob', 'linewidth', 2);
 hold on;
 xlabel('x');
-ylabel('\rho=x^3');
+%ylabel('\rho=x^3');
+ylabel('\rho=x^2');
 set(gca, "linewidth", 2, "fontsize", 14)
 
 % gradient
-yp = 6*x; % Analytical expression
+yp = 3*x.^2; % Analytical expression
 
-%yp_forwardDifference = zeros(size(y));
+yp_forwardDifference = zeros(size(y));
 yp_central2ndOrderDerivative = zeros(size(y));
 
 %yp_n1(1, 1) = (y(2, 1) - y(1, 1)) / (x(2, 1) - x(1, 1));
 %yp_n1(n, 1) = (y(n, 1) - y(n-1, 1)) / (x(n, 1) - x(n-1, 1));
 
 for i = 2 : length(y)-1
-  %yp_forwardDifference(i, 1) = (y(i+1, 1) - y(i, 1)) / (x(i+1, 1) - x(i, 1));
-  yp_central2ndOrderDerivative(i, 1) = (y(i+1, 1) - (2*y(i, 1)) + y(i-1, 1)) / (x(i+1, 1) - x(i, 1)).^2;
+  yp_forwardDifference(i, 1) = (y(i+1, 1) - y(i, 1)) / (x(i+1, 1) - x(i, 1));
+  yp_centralDifference(i, 1) = (y(i+1, 1) - y(i-1, 1)) / (x(i+1, 1) - x(i-1, 1));
+  %yp_central2ndOrderDerivative(i, 1) = (y(i+1, 1) - (2*y(i, 1)) + y(i-1, 1)) / (x(i+1, 1) - x(i, 1)).^2;
 end
 
 figure(2);
 hold on;
 plot(x(2:n-1), yp(2:n-1), '-b', 'linewidth', 2);
-%plot(x(2:n-1), yp_forwardDifference(2:n-1), '--om', 'linewidth', 2);
-plot(x(2:n-1), yp_central2ndOrderDerivative(2:n-1), 'ov', 'linewidth', 2);
-%legend('analytical', 'forwardDifference', 'centraldifference', 'location', 'northwest');
-legend('analytical', '2ndOrderCentralDifference', 'location', 'northwest');
+plot(x(2:n-1), yp_forwardDifference(2:n-1), '--om', 'linewidth', 2);
+plot(x(2:n-1), yp_centralDifference(2:n-1), '--ov', 'linewidth', 2);
+%plot(x(2:n-1), yp_central2ndOrderDerivative(2:n-1), 'ov', 'linewidth', 2);
+%legend('analytical', 'forwardDifference', 'location', 'northwest');
+legend('analytical', 'forwardDifference', 'centraldifference', 'location', 'northwest');
+%legend('analytical', '2ndOrderCentralDifference', 'location', 'northwest');
 hold on;
 xlabel('x');
-ylabel('d^2\rho/dx^2=6x');
+%ylabel('d^2\rho/dx^2=2x');
+ylabel('d\rho/dx=2x');
 box on;
 set(gca, "linewidth", 2, "fontsize", 14)
 hold off;
